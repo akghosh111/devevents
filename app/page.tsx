22a4355abc687ba4734f75c677b0f9bc430899ea
akghosh111 <ghoshkiran2017@gmail.com>
2025-10-26 14:51:44 +0530

    added explore button as client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from "@/components/ExploreBtn"
 
 
 const Page = () => {
@@ -5,6 +6,8 @@ const Page = () => {
     <section>
       <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn/>
     </section>
   )
 }
